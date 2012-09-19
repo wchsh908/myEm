200b6d0a864d98abe0f9e6b45feb926e859fb73e
root <root@mta1.gdedm.com>
2012-09-20 00:43:57 +0800

    v0.17

--- tools/sql/subscriber_bounces.sh
+++ tools/sql/subscriber_bounces.sh
@@ -3,4 +3,4 @@
 tempfile=/tmp/tmpdboutput
  
 sql='select B.emailaddress,A.bouncetype,A.bouncerule  from emarketer.email_list_subscriber_bounces as A left join emarketer.email_list_subscribers as B on A.subscriberid = B.subscriberid;'
-echo sql | mysql -u root -pwchsh908 > /var/www/html/sqlresult.txt
+echo $sql | mysql -u root -pwchsh908 > /var/www/html/sqlresult.txt
