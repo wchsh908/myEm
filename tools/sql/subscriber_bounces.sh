2f1867f6dff19d0e25b8881a3ab18e86fc8fbe0e
root <root@218.98.60.2>
2012-09-25 19:00:08 +0800

    0.31

--- tools/sql/subscriber_bounces.sh
+++ tools/sql/subscriber_bounces.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 tempfile=/tmp/tmpdboutput
-if [ -f /var/www/html/tools/sql/lastbounceid]
+if [ -f /var/www/html/tools/sql/lastbounceid ]
 then
 	lastbounceids=($(cat /var/www/html/tools/sql/lastbounceid))
 	lastbounceid=${lastbounceids[1]}
 fi
-if (( $lastbounceid <= 0 ))
+if (( $(( $lastbounceid )) <= 0 ))
 then 
 	lastbounceid=0
 fi
 
-sql="select B.emailaddress,A.bouncetype,A.bouncerule  from emarketer.email_list_subscriber_bounces as A left join emarketer.email_list_subscribers as B on A.subscriberid = B.subscriberid where bounceid > $lastbounceid;"
+sql="select B.emailaddress,A.bouncetype,A.bouncerule  from emarketer.email_list_subscriber_bounces as A left join emarketer.email_list_subscribers as B on A.subscriberid = B.subscriberid where A.bounceid > $lastbounceid;"
 echo $sql | mysql -u root -pwchsh908 > /var/www/html/sqlresult.txt
 
-sql="select max(bounceid) from emarketer.email_list_subscribers_bounces";
+sql="select max(bounceid) from emarketer.email_list_subscriber_bounces";
 echo $sql | mysql -u root -pwchsh908 > /var/www/html/tools/sql/lastbounceid
