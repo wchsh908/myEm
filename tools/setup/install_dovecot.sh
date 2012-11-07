4c024adc3f9fbf71653a5c134b31e0f28c3918fc
root <root@218.98.60.2>
2012-11-07 09:50:11 +0800

    v0.42

---
+++ tools/setup/install_dovecot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "正在备份账号密码..."
+if [ -f /etc/dovecot/passwd ]; then
+	mv /etc/dovecot/passwd /var/passwd
+else
+	echo > /var/passwd
+
+echo "正在删除dovecot..."
+yum -y erase dovecot
+echo "正在重装dovecot..."
+yum -y install dovecot
+chkconfig dovecot on
+
+
+echo "正在配置dovecot..."
+cat dovecot.conf > /etc/dovecot.conf
+
+echo "正在还原密码..."
+mkdir /etc/dovecot
+mv /var/passwd /etc/dovecot/passwd
+
+echo "正在重启dovecot..."
+service dovecot restart
+
