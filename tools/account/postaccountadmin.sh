372b045c9e13a812e57d0c034e06811bf2f0b483
jlugene <jlugene@qq.com>
2012-11-13 15:15:52 +0800

    v.046

--- tools/account/postaccountadmin.sh
+++ tools/account/postaccountadmin.sh
@@ -7,7 +7,8 @@ showusage()
 	echo 'Usage:'
 	echo './postaccountadmin.sh -s:  Show all current domains and mail accounts;'
 	echo './postaccountadmin.sh -h:  Show help for this script;'
-	echo './postaccountadmin.sh -i:  Auto create accounts made by emailmarketer;'
+	echo './postaccountadmin.sh -i:  Auto create accounts from file /tmp/newaccount.txt (made by emailmarketer);'
+	echo './postaccountadmin.sh -c:  Clear text of file /tmp/newaccount.txt;'
 	echo './postaccountadmin.sh -a user@domain:  Add a mail account;'
 	echo './postaccountadmin.sh -a filename:  Add mail accounts from a file(full path);'
 	echo './postaccountadmin.sh -d user@domain: Delete a mail account;'
@@ -251,6 +252,9 @@ if (( $# == 1 )) ; then
 		exit 0
 	elif [ ${1} = '-i' ]; then
 		/var/www/html/tools/account/postaccountadmin.sh -a /tmp/newaccount.txt
+		cat /dev/null > /tmp/newaccount.txt
+		exit 0
+	elif [ ${1} = '-c' ]; then
 		cat /dev/null > /tmp/newaccount.txt
 		exit 0
 	fi
