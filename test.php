881dca702908277b44702139eb133f12bd605553
jlugene <jlugene@qq.com>
2012-11-13 14:49:09 +0800

    v.046

--- test.php
+++ test.php
@@ -1,34 +1,45 @@
 <?php 
 echo "hi,this is a test page for php.<br/>";
 
-//字符库0-9,a-z
-$charray = range('a', 'z');
-for ($i = 0; $i < 10; $i++)
-{
-	$charray[$i + 26] = $i;
-}
-
-//打开读
-$domainarray = file("/etc/postfix/vdomains");
-$strjoin="";
-foreach ($domainarray as $domain)
-{
-	$str = "bo_";
-	$count = rand(10, 15);//长度
-	for ($i = 0; $i < $count; $i++)
+	//2012-Sep-23, added by jinxiaohu
+	private function getnewEaddress()
 	{
-		$index = rand(0, 35);
-		$str .= $charray[$index];
+		//字符库0-9,a-z
+		$charray = range('a', 'z');
+		for ($i = 0; $i < 10; $i++)
+		{
+			$charray[$i + 26] = $i;
+		}
+
+		//打开读
+		$domainarray = file("/etc/postfix/vdomains");
+		if (count($domainarray) == 0)
+			return "Error,can not open /etc/postfix/vdomains";
+		$strjoin="";
+		foreach ($domainarray as $domain)
+		{
+			if (ereg ('^([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$', $domain))
+			{
+				$str = "bo_";
+				$count = rand(10, 15);//长度
+				for ($i = 0; $i < $count; $i++)
+				{
+					$index = rand(0, 35);
+					$str .= $charray[$index];
+				}
+				$str .= "@".$domain;
+				
+				if ($strjoin == "")
+					$strjoin .= $str;	
+				else
+					$strjoin .= ";".$str;
+			}
+		}
+		return $strjoin;
 	}
-	$str .= "@".$domain;
-	
-	if ($strjoin == "")
-		$strjoin .= $str;	
-	else
-		$strjoin .= ";".$str;
-}
-echo $strjoin;
-echo "<br/>";
+
+$strr = getnewEaddress();
+echo $strr;
 	
 //打开写
 $fp = fopen("/var/tmp/newaccount.txt", 'a');
