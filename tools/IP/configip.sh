8ba251d46acbf31b7708c0435d4340365616f85f
jlugene <jlugene@qq.com>
2012-11-29 14:23:19 +0800

    v0.49

--- tools/IP/configip.sh
+++ tools/IP/configip.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
 
+if ip route | grep "default";then
+	option=$(ip route | grep "default")		
+	option=$(ip route | grep "default")
+	option=${option%src*}
+else
+	echo "Can not change ip ,stop."
+	exit 1
+fi
+
+
 IPs=($(cat srcIP.txt))
 count=${#IPs[*]}
 dftIP=${IPs[0]}
 
-i=1
+rm -f /etc/sysconfig/network-scripts/ifcfg-eth0:*	
 
-rm -f /etc/sysconfig/network-scripts/ifcfg-eth0:*
+i=1
 while (( $i < $count ))
 do
 	IP=${IPs[$i]}
 	sed -e "s/eth0/eth0:$i/;s/$dftIP/$IP/"  /etc/sysconfig/network-scripts/ifcfg-eth0 >  "/etc/sysconfig/network-scripts/ifcfg-eth0:$i"
 	i=$(($i+1))
 done
+
+ip route change $option src $dftIP
